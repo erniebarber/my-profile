df9322dac64b29c70f8373705b0ee1b32a45c0fa
Ernie Barber <erniebarber@me.com>
2018-09-12 22:53:07 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -17,8 +17,11 @@ pc = portal.Context()
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
  
-# Add a raw PC to the request.
-node = request.RawPC("node")
+# Add a raw PC to the request.  CHANGED: to XenVM
+node = request.XenVM("node")
+
+# add CentOS image to the Node
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
