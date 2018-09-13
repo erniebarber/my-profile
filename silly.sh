d646df432a00ea40eaf9431a2077cd6bb984e03b
Ernie Barber <erniebarber@me.com>
2018-09-13 15:40:59 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -7,19 +7,16 @@ sudo yum -y update
 # install apaches
 sudo yum install httpd
 
-
-sudo systemctl stop httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
 #allowApache trafic through firewall
 sudo firewall-cmd --permanent --add-port=80/tcp
 sudo firewall-cmd --permanent --add-port=443/tcp
 sudo firewall-cmd --reload
 
-sudo systemctl status httpd
+sudo systemctl stop httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
-sudo httpd -v
+
 
 
 
