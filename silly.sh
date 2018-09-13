4da6d55d9ddd87f530c31f7028cf50edd9850fa0
Ernie Barber <erniebarber@me.com>
2018-09-13 14:47:33 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -5,13 +5,15 @@
 sudo yum -y update
 
 # install apaches
-sudo firewall-cmd --permanent --add-port=80/tcp
-sudo firewall-cmd --permanent --add-port=443/tcp
+sudo yum install httpd
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+
+sudo firewall-cmd --zone=public --permanent --add-service=http
+sudo firewall-cmd --zone=public --permanent --add-service=https
 sudo firewall-cmd --reload
 
-sudo systemctl install httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+
 
 echo "This is a silly script" > /tmp/silly.txt
 
