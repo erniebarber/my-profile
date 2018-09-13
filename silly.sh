a180b3a814487daf0d9db4baac926a3802357dc8
Ernie Barber <erniebarber@me.com>
2018-09-13 13:24:43 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sudo install apache2
-#update a package
+
+#update the package
+sudo yum -y update
+
+# inistall lapache
+#sudo firewall-cmd --permanent --add-port=80/tcp
+#sudo firewall-cmd --permanent --add-port=443/tcp
+#sudo firewall-cmd --reload
+
+sudo systemctl install httpd
+#sudo systemctl start httpd
+#sudo systemctl enable httpd
+
 echo "This is a silly script" > /tmp/silly.txt
 
