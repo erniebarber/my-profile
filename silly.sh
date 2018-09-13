d26cccddf35e4b814ed931b9382d868f3c013941
Ernie Barber <erniebarber@me.com>
2018-09-13 19:15:34 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "This is a Apache installation script" > /tmp/silly.txt
+#echo "This is a Apache installation script" > /tmp/silly.txt
 
 sudo yum update -y
 sudo yum install -y httpd
